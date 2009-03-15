a18ca16b0a6f56865653df86fe06fd09c28de584
ntatsumi <ntatsumi@5cfc46f6-f562-11dd-9d1d-173cc1d06e3c>
2009-03-15 20:59:41 +0000

    JavaObjectProfiler added in session.util package

---
+++ trunk/sessionmon/SessionMon/src/sessionmon/util/JavaObjectProfiler.java
@@ -0,0 +1,73 @@
+package sessionmon.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import com.vladium.utils.IObjectProfileNode;
+import com.vladium.utils.ObjectProfiler;
+
+public class JavaObjectProfiler {
+	private Object obj;
+	private ByteArrayOutputStream bs;
+	private ObjectOutputStream os;
+	
+	private String objectType;
+	private int objectGraphSizeInBytes;
+	private int objectSerializedSizeInBytes;
+	private String toStringValue;
+	
+	public JavaObjectProfiler(Object o) throws IllegalArgumentException {
+		if(o == null)
+			throw new IllegalArgumentException();
+		set(o);
+	}
+	
+	public void set(Object o) {
+		clear();
+		this.obj = o;
+		init();
+	}
+	
+	public void clear() {
+		this.obj = null;
+		this.bs = null;
+		this.os = null;
+		this.objectType = null;
+		this.objectGraphSizeInBytes = 0;
+		this.objectSerializedSizeInBytes = 0;
+		this.toStringValue = null;
+	}
+	
+	private void init() {
+		bs = new ByteArrayOutputStream();
+		try {
+			os = new ObjectOutputStream(bs);
+			os.writeObject(obj);
+			objectSerializedSizeInBytes = bs.size();
+			objectType = obj.getClass().getName();
+			toStringValue = obj.toString();
+			
+			IObjectProfileNode profile = ObjectProfiler.profile (obj);
+			objectGraphSizeInBytes = profile.size();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getObjectType() {
+		return objectType;
+	}
+
+	public int getObjectGraphSizeInBytes() {
+		return objectGraphSizeInBytes;
+	}
+
+	public int getObjectSerializedSizeInBytes() {
+		return objectSerializedSizeInBytes;
+	}
+
+	public String getToStringValue() {
+		return toStringValue;
+	}
+}
