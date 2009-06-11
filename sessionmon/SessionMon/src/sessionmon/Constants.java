d7bbd1bc0b688eb0e00c72499fb485207d273f68
ntatsumi <ntatsumi@5cfc46f6-f562-11dd-9d1d-173cc1d06e3c>
2009-06-11 03:02:50 +0000

    Tracking of active sessions refactored.

--- sessionmon/SessionMon/src/sessionmon/Constants.java
+++ sessionmon/SessionMon/src/sessionmon/Constants.java
@@ -6,7 +6,7 @@ public interface Constants {
 	public static final SimpleDateFormat DATE_FORMAT_DATE_TIME = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss:S");
 	public static final SimpleDateFormat DATE_FORMAT_DAY_OF_WEEK_TIME_YEAR = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyy");
 	
-	public static final String CONTEXT_PARAMETER_TOTAL_NUMBER_OF_ACTIVE_SESSIONS = "sessionmon.numOfActiveSessions";
+	public static final String CONTEXT_PARAMETER_ACTIVE_SESSIONS = "sessionmon.activeSessions";
 	public static final String CONTEXT_PARAMETER_LAST_ATTRIBUTE_UPDATE_TIME = "sessionmon.lastAttributeUpdateTime";
 	public static final String CONTEXT_PARAMETER_CONFIGURATION = "sessionmon.config";
 	
